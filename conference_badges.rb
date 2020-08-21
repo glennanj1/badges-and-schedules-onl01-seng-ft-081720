6885c604abb952fb8a96b16e36e4a4f68ebe7552
John Glennan <glennan20@gmail.com>
2020-08-21 14:46:28 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,27 @@
 # Write your code here.
+
+def badge_maker(name)
+  "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(names)
+  messages = []
+  names.each do |name|
+    messages << "Hello, my name is #{name}."
+  end
+  messages
+end
+
+def assign_rooms                   (speakers)
+  speakers.each do |speaker, index|
+    "Hello      #{attendee}! You'll be assigned to room #{index+1}"
+  end
+  
+end
+
+
+  
+  
+    
+  
+  
