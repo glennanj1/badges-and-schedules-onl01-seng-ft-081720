56f7089160067047e8e6aa38dfbbc1bd85446e6b
John Glennan <glennan20@gmail.com>
2020-08-21 14:51:06 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,10 +13,10 @@ def batch_badge_creator(names)
 end
 
 def assign_rooms(speakers)
-  speakers.each_with_index do |speaker, index|
-    message = "Hello, #{speaker}! You'll be assigned to room #{index+1}"
+  speakers.each_with_index.map do |speaker, index|
+    "Hello, #{speaker}! You'll be assigned to room #{index+1}"
   end
-  message
+  
 end
 
 
